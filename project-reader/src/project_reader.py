27beb69a847eb8bfae8b62de9aa13358ca38d0ce
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-01-16 15:25:07 +0200

    Add

---
+++ project-reader/src/project_reader.py
@@ -0,0 +1,21 @@
+from urllib import request
+from project import Project
+import toml
+
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+        parsed = toml.loads(content)
+        poetry = parsed['tool']['poetry']
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project(
+            poetry['name'],
+            poetry['description'],
+            poetry['dependencies'],
+            poetry['dev-dependencies'])
