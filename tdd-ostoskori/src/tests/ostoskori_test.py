9ce2e4ab2b8d1b06719202d61e9c9d2e3bf2364a
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-01-18 12:05:16 +0200

    Add test for 2 different products

--- tdd-ostoskori/src/tests/ostoskori_test.py
+++ tdd-ostoskori/src/tests/ostoskori_test.py
@@ -19,3 +19,10 @@ class TestOstoskori(unittest.TestCase):
         maito = Tuote("Maito", 3)
         self.kori.lisaa_tuote(maito)
         self.assertEqual(self.kori.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        pirkka = Tuote("Pirkka-olut", 1)
+        self.kori.lisaa_tuote(pirkka)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
