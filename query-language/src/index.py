805b8037d53ccdd93d61825f8fa5d53bfb45e89d
Mikko Kallio <mikko.m.kallio@gmail.com>
2022-01-22 12:16:57 +0200

    Complete

--- query-language/src/index.py
+++ query-language/src/index.py
@@ -1,46 +1,34 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, All, HasAtLeast, HasFewerThan, Not, Or, PlaysIn
+from querybuilder import QueryBuilder
+
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasFewerThan(5, "goals"),
-        PlaysIn("EDM")
+    query = QueryBuilder()
+
+    m1 = (
+        query
+        .playsIn("PHI")
+        .hasAtLeast(10, "assists")
+        .hasFewerThan(5, "goals")
+        .build()
     )
 
-    matcher = And(
-        Not(HasAtLeast(1, "goals")),
-        PlaysIn("NYR")
-    )
-    
-    matcher = And(
-        HasFewerThan(1, "goals"),
-        PlaysIn("NYR")
+    m2 = (
+        query
+        .playsIn("EDM")
+        .hasAtLeast(40, "points")
+        .build()
     )
 
-    matcher = All()
-    
-    matcher = Or(
-        HasAtLeast(30, "goals"),
-        HasAtLeast(50, "assists")
-    )
-
-    matcher = And(
-        HasAtLeast(40, "points"),
-        Or(
-            PlaysIn("NYR"),
-            PlaysIn("NYI"),
-            PlaysIn("BOS")
-        )
-    )
+    matcher = query.oneOf(m1, m2).build()
 
     for player in stats.matches(matcher):
         print(player)
-
 
 if __name__ == "__main__":
     main()
---
+++ query-language/src/querybuilder.py
@@ -0,0 +1,21 @@
+from matchers import All, And, HasAtLeast, HasFewerThan, Or, PlaysIn
+
+
+class QueryBuilder:
+    def __init__(self, query = All()):
+        self.query = query
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self.query, PlaysIn(team)))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(self.query, HasAtLeast(value, attr)))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(self.query, HasFewerThan(value, attr)))
+
+    def oneOf(self, *queries):
+        return QueryBuilder(Or(*queries))
+
+    def build(self):
+        return self.query
